273bfaa521bacd3a2a9bc0ff6c38e533285bad17
Marius Tolzmann <marius@mineiros.io>
2022-04-08 17:33:36 +0200

    feat: add staging SA

---
+++ stacks/staging/service-accounts/cloud-run/_terramate_generated_providers.tf
@@ -0,0 +1,18 @@
+// TERRAMATE: GENERATED AUTOMATICALLY DO NOT EDIT
+// TERRAMATE: originated from generate_hcl block on /stacks/providers.tm.hcl
+
+provider "google" {
+  project = "mineiros-terramate-staging"
+  region  = "europe-north1"
+}
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.0"
+    }
+  }
+}
+terraform {
+  required_version = "~> 1.1.0"
+}
